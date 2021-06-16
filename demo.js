6455afea72ced101700b8683c627538e278a6243
freebrunch <scudcoding@gmail.com>
2021-06-15 19:49:21 -0700

    added sort to demo.js

--- demo.js
+++ demo.js
@@ -15,4 +15,24 @@ function twoSumWithHash(arr, target) {
   return [];
 }
 
-console.log(twoSumWithHash([5, 2, 3,3], 6))
+
+const twoSumWithSort = (arr, target) => {
+  if (!arr || arr.length === 0) return [];
+  // sort the array
+  arr.sort((a, b) => a - b);
+  for (let i = 0, j = arr.length - 1; i <= j ; ) {
+    const sum = arr[i] + arr[j];
+    if (sum === target) {
+      return true;
+    } else if (sum > target) {
+      j--;
+    } else {
+      i++;
+    } 
+  }
+  return false;
+}
+
+const array = [5, 2, 3, 3], target = 6;
+console.log(twoSumWithHash(array, target))
+console.log(twoSumWithSort(array, target))
